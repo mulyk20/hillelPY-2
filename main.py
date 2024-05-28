e5364ac6a9112fe17ec1895022a04542e8470d3b
alex <alexmulyk20@gmail.com>
2024-05-28 19:40:59 +0300

    seven

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from utils import email_sender
 
-print("from main", __name__)
-
 
 def main():
-    email_sender.send_email(recipients=["test_hillel_api_mailing@ukr.net"], mail_body="fffffff", mail_subject="gggggg")
+    inner_html_body = email_sender.create_welcome_letter(
+        {
+            'name': 'Alex Mulyk',
+            'hobbies': ['volleyball', 'soccer'],
+            'has_car': True
+        }
+    )
+    print(inner_html_body)
+    email_sender.send_email(
+        ["test_hillel_api_mailing@ukr.net", "dduckker@ukr.net"],
+        mail_body=inner_html_body,
+        mail_subject="mail_subject",
+        # attachment='2024-05-21_199-25.png',
+    )
 
 
 if __name__ == "__main__":

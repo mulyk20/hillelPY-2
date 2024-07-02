d0b7c6eef78c951a7d9d1c4ef8464a931b698583
alex <alexmulyk20@gmail.com>
2024-07-02 19:52:09 +0300

    tourhw

--- main.py
+++ main.py
@@ -19,7 +19,6 @@
 
 
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
